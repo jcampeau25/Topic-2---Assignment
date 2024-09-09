4f8932642b338e94006095c00416e2f11a002272
jcampeau25 <jdccanuck25@gmail.com>
2024-09-09 09:01:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Topic_2___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Josh Campeau
+            Console.WriteLine("My age is " + (2024 - 2008));
+            Console.WriteLine();
+            Console.WriteLine("The average of 14, 15, and 16 is " + ((14 + 15 + 16) / 3));
+            Console.WriteLine();
+            Console.WriteLine("The average of 14, 15, 16, and 17 is " + ((14.0 + 15.0 + 16.0 + 17.0) / 4));
+            Console.WriteLine();
+            Console.WriteLine("The area of a circle with a radius of 3 cm is " + (Math.PI * 3 + "cm"));
+            Console.WriteLine();
+            Console.WriteLine("C# knows whether to add or concatenate depedning on what type of code the program is dealing with, if dealing with strings the program will cocatenate, if dealing with integers or varaibles, the program will add");
+            Console.ReadLine();
+
+        }
+    }
+}
